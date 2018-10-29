23910d6a2fca90b52b391b5c7b6dff93cae674eb
Sebastian Schulz <sebastianschulz@gmx.com>
2018-10-29 14:06:31 +0100

    Deleted old config files.

--- kernel_test.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-from astropy.convolution import Gaussian2DKernel
-gaussian_2D_kernel = Gaussian2DKernel(10, y_stddev=5, )
-plt.imshow(gaussian_2D_kernel, interpolation='none', origin='lower')
-plt.xlabel('x [pixels]')
-plt.ylabel('y [pixels]')
-plt.colorbar()
-plt.show()
